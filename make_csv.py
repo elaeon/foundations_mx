eddc783ca9e112c6fab638def23252f0c3d7c568
Alejandro <elaeon@gmail.com>
2026-04-03 17:53:37 -0600

    fix make_csv

--- make_csv.py
+++ make_csv.py
@@ -51,6 +51,12 @@ def read_excel_and_save_csv(sheet_columns_dict, empty_cols, data_folder="data", 
         print(f"Processing {excel_file.name}...")
         
         # Process each sheet and column configuration
+        df_folio = pl.read_excel(
+            excel_file,
+            sheet_name="Carátula",
+            columns="Folio"
+        )
+        df_folio = df_folio.sort("Folio").select(pl.last("Folio"))
         for sheet_name, columns in sheet_columns_dict.items():
             try:
                 # Read the Excel sheet with specified columns
@@ -58,17 +64,23 @@ def read_excel_and_save_csv(sheet_columns_dict, empty_cols, data_folder="data", 
                     excel_file,
                     sheet_name=sheet_name,
                     columns=columns
-                )
-                
+                ).join(df_folio, on="Folio")
                 if excel_file.name in df_dict:
                     if sheet_name in ["Carátula", "Generales", "Nómina"] and df.shape[0] == 1:
-                        df_dict[excel_file.name] = df_dict[excel_file.name].join(df, how="cross")
+                        df_dict[excel_file.name] = df_dict[excel_file.name].join(df, on="Folio")
+                        if sheet_name == "Generales":
+                            df_dict[excel_file.name]  = df_dict[excel_file.name].cast(
+                               {"Activo circulante": pl.Float64, "Activo fijo": pl.Float64, "Activo diferido": pl.Float64, "Pasivo": pl.Float64, "Patrimonio": pl.Float64})
+                        elif sheet_name == "Nómina":
+                            df_dict[excel_file.name]  = df_dict[excel_file.name].cast(
+                               {"Monto salarios": pl.Float64})
                     else:
                         df = df.with_columns(pl.lit(1).alias("key"))
                         if sheet_name == "Órgano de gobierno":
                             df = df.group_by("key").agg([pl.col("Nombre integrante"), pl.col("Monto salario").sum()])
                             df = df.with_columns(pl.col("Nombre integrante").list.join(","))
                             df = df.rename({"Nombre integrante": "Integrantes del órgano de gobierno"})
+                            df = df.cast({"Monto salario": pl.Float64})
                         elif sheet_name == "Ingreso por donativos":
                             df = df.group_by("key").agg([
                                 pl.col("Monto efectivo").sum(), 
@@ -78,12 +90,14 @@ def read_excel_and_save_csv(sheet_columns_dict, empty_cols, data_folder="data", 
                                 "Monto efectivo": f"{sheet_name}_Monto efectivo",
                                 "Monto especie": f"{sheet_name}_Monto especie"
                             })
+                            df = df.cast({f"{sheet_name}_Monto efectivo": pl.Float64, f"{sheet_name}_Monto especie": pl.Float64})
                         elif sheet_name == "Destino de donativos":
                             df = df.group_by("key").agg([
                                 pl.col("Monto").sum(), 
                                 pl.col("Número de beneficiados").sum()
                             ])
                             df = df.rename({"Monto": f"{sheet_name}_Monto"})
+                            df = df.cast({f"{sheet_name}_Monto": pl.Float64})
                         elif sheet_name == "Gastos":
                             df = df.group_by("key").agg([
                                     pl.col("Monto nacional operación").sum(), 
@@ -91,29 +105,36 @@ def read_excel_and_save_csv(sheet_columns_dict, empty_cols, data_folder="data", 
                                     pl.col("Monto extranjero operación").sum(),
                                     pl.col("Monto extranjero admin").sum()
                                 ])
+                            df = df.cast(pl.Float64)
                         elif sheet_name == "Donativos otorgados":
                             df = df.group_by("key").agg([
                                     pl.col("Monto efectivo").sum(), 
                                     pl.col("Monto especie").sum()
                                 ])
+                            df = df.rename({
+                                "Monto efectivo": f"{sheet_name}_Monto efectivo",
+                                "Monto especie": f"{sheet_name}_Monto especie"
+                            })
+                            df = df.cast({f"{sheet_name}_Monto efectivo": pl.Float64, f"{sheet_name}_Monto especie": pl.Float64})
                         elif sheet_name == "Ingresos relacionados":
                             df = df.group_by("key").agg([
                                     pl.col("Monto").sum()
                                 ])
                             df = df.rename({"Monto": f"{sheet_name}_Monto"})
+                            df = df.cast({f"{sheet_name}_Monto": pl.Float64})
                         elif sheet_name == "Ingresos no relacionados":
                             df = df.group_by("key").agg([
                                     pl.col("Monto").sum()
                                 ])
                             df = df.rename({"Monto": f"{sheet_name}_Monto"})
+                            df = df.cast({f"{sheet_name}_Monto": pl.Float64})
                         df = df.select(pl.exclude("key"))
                         df_dict[excel_file.name] = df_dict[excel_file.name].join(df, how="cross")
                 else:
                     df_dict[excel_file.name] = df
-            except Exception as e:
+            except ValueError as e:
                 df = pl.DataFrame(empty_cols[sheet_name])
                 df_dict[excel_file.name] = df_dict[excel_file.name].join(df, how="cross")
-                #print(f"✗ Error reading sheet '{sheet_name}' from {excel_file.name}: {e}")
 
         df_dict[excel_file.name] = df_dict[excel_file.name].with_columns(
             pl.lit(str(excel_file)).alias("ref")
@@ -130,33 +151,33 @@ def read_excel_and_save_csv(sheet_columns_dict, empty_cols, data_folder="data", 
 def main():
     # Example usage
     sheet_config = {
-        "Carátula": ["Rfc", "Razón social", "Rubro"],
-        "Generales": ["Misión", "Valores", "Actividad", "Activo circulante", "Activo fijo", "Activo diferido", "Pasivo", "Patrimonio"],
-        "Ingreso por donativos": ["Donante", "Monto efectivo", "Monto especie"],
-        "Donativos otorgados": ["Rfc destinatario", "Monto efectivo", "Monto especie"],
-        "Órgano de gobierno": ["Nombre integrante", "Puesto", "Monto salario", "Tipo integrante"],
-        "Nómina": ["Plantilla laboral", "Voluntarios", "Monto salarios"],
-        "Ingresos relacionados": ["Ingresos relacionados_Monto"],
-        "Ingresos no relacionados": ["Ingresos no relacionados_Monto"],
-        "Destino de donativos": ["Concepto", "Sector beneficiado", "Monto", "Número de beneficiados", "Entidad federativa", "Municipio"],
-        "Gastos": ["Concepto", "Especifique", "Monto nacional operación", "Monto nacional admin", "Monto extranjero operación", "Monto extranjero admin"]
+        "Carátula": ["Folio","Rfc", "Razón social", "Rubro"],
+        "Generales": ["Folio","Misión", "Valores", "Actividad", "Activo circulante", "Activo fijo", "Activo diferido", "Pasivo", "Patrimonio"],
+        "Ingreso por donativos": ["Folio","Donante", "Monto efectivo", "Monto especie"],
+        "Donativos otorgados": ["Folio","Rfc destinatario", "Monto efectivo", "Monto especie"],
+        "Órgano de gobierno": ["Folio","Nombre integrante", "Puesto", "Monto salario", "Tipo integrante"],
+        "Nómina": ["Folio","Plantilla laboral", "Voluntarios", "Monto salarios"],
+        "Ingresos relacionados": ["Folio","Monto"],
+        "Ingresos no relacionados": ["Folio","Monto"],
+        "Destino de donativos": ["Folio","Concepto", "Sector beneficiado", "Monto", "Número de beneficiados", "Entidad federativa", "Municipio"],
+        "Gastos": ["Folio","Concepto", "Especifique", "Monto nacional operación", "Monto nacional admin", "Monto extranjero operación", "Monto extranjero admin"]
     }
     agg_names = {
         "Carátula": ["Rfc", "Razón social", "Rubro"],
         "Generales": ["Misión", "Valores", "Actividad", "Activo circulante", "Activo fijo", "Activo diferido", "Pasivo", "Patrimonio"],
         "Ingreso por donativos": ["Ingreso por donativos_Monto efectivo", "Ingreso por donativos_Monto especie"],
-        "Donativos otorgados": ["Monto efectivo", "Monto especie"],
+        "Donativos otorgados": ["Donativos otorgados_Monto efectivo", "Donativos otorgados_Monto especie"],
         "Órgano de gobierno": ["Integrantes del órgano de gobierno", "Monto salario"],
         "Nómina": ["Plantilla laboral", "Voluntarios", "Monto salarios"],
-        "Ingresos relacionados": ["Concepto", "Monto"],
-        "Ingresos no relacionados": ["Concepto libre", "Monto"],
+        "Ingresos relacionados": ["Ingresos relacionados_Monto"],
+        "Ingresos no relacionados": ["Ingresos no relacionados_Monto"],
         "Destino de donativos": ["Destino de donativos_Monto", "Número de beneficiados"],
         "Gastos": ["Monto nacional operación", "Monto nacional admin", "Monto extranjero operación", "Monto extranjero admin"]
     }
     
     empty_cols = {}
     for key, columns in agg_names.items():
-        empty_cols[key] = {col: [1] for col in columns}
+        empty_cols[key] = {col: [0.0] for col in columns}
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--year", required=True, type=int)
